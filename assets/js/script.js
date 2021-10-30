7af652f77a1a77d824bf28c2d0ae81a97cc12aa6
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-30 19:26:52 +0800

    Add event listeners to each btn tip

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 const billNumber = document.querySelector('#bill__number');
 const tipNumberCustom = document.querySelector('#tip__number_custom');
+const btnsTipNumber = document.querySelectorAll('.btn_tip')
 const tipNumber5 = document.querySelector('#tip__number_5');
 const tipNumber10 = document.querySelector('#tip__number_10');
 const tipNumber15 = document.querySelector('#tip__number_15');
@@ -11,7 +12,18 @@ const peopleNumber = document.querySelector('#people_number');
 const btnReset = document.querySelector('#btn__reset');
 
 
-billNumber.addEventListener('input', countBill);
-tipNumberCustom.addEventListener('input', countTipCustom);
-peopleNumber.addEventListener('input', countPeople);
-btnReset.addEventListener('click', resetAll);
+// billNumber.addEventListener('input', countBill);
+// tipNumberCustom.addEventListener('input', countTipCustom);
+// peopleNumber.addEventListener('input', countPeople);
+// btnReset.addEventListener('click', resetAll);
+
+let selectedTip = 0;
+
+btnsTipNumber.forEach((btnTipNumber) =>
+  btnTipNumber.addEventListener('click', (e) => {
+    e.preventDefault();
+    selectedTip = Number.parseInt(e.target.dataset.tip);
+    // console.log(e.target.dataset.tip + " event target");
+    // console.log(selectedTip + " selected TIP");
+  })
+)
