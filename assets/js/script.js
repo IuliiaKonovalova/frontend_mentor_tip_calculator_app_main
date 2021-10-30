e8994a54570c5cfcbca7fa7f6d723af751814e58
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-30 16:11:18 +0800

    Add variable for reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 //variables
 
-const billNumber = document.querySelector('#bill__number')
-const tipNumberCustom = document.querySelector('#tip__number_custom')
-const tipNumber5 = document.querySelector('#tip__number_5')
-const tipNumber10 = document.querySelector('#tip__number_10')
-const tipNumber15 = document.querySelector('#tip__number_15')
-const tipNumber25 = document.querySelector('#tip__number_25')
-const tipNumber50 = document.querySelector('#tip__number_50')
-const peopleNumber = document.querySelector('#people_number')
+const billNumber = document.querySelector('#bill__number');
+const tipNumberCustom = document.querySelector('#tip__number_custom');
+const tipNumber5 = document.querySelector('#tip__number_5');
+const tipNumber10 = document.querySelector('#tip__number_10');
+const tipNumber15 = document.querySelector('#tip__number_15');
+const tipNumber25 = document.querySelector('#tip__number_25');
+const tipNumber50 = document.querySelector('#tip__number_50');
+const peopleNumber = document.querySelector('#people_number');
+const btnReset = document.querySelector('#btn__reset')
