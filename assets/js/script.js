9c292cb10e8735e8a15f44fec4ed9c7450896806
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-31 13:11:30 +0800

    Add btn focus for tip input

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,9 @@ const tipNumber50 = document.querySelector('#tip__number_50');
 const peopleNumber = document.querySelector('#people_number');
 const btnReset = document.querySelector('#btn__reset');
 
+const personAmount = document.querySelector('#person__amount');
+const totalAmount = document.querySelector('#total__amount')
+
 const warningMessage = document.querySelector('#warning')
 
 
@@ -22,57 +25,80 @@ document.addEventListener('DOMContentLoaded', function () {
   tipNumberCustom.addEventListener('input', countTipCustom);
   peopleNumber.addEventListener('input', countPeople);
   btnReset.addEventListener('click', resetAll);
-  btnsTipNumber.forEach((btnTipNumber) =>
-    btnTipNumber.addEventListener('click', (e) => {
-      e.preventDefault();
-      selectedTip = Number.parseInt(e.target.dataset.tip);
-      // console.log(e.target.dataset.tip + " event target");
-      // console.log(selectedTip + " selected TIP");
-      tip = selectedTip
-    })
-  )
-  calculation()
+
 });
 
 const integer = /\d{1,}/;
-tip = 0
-
-let selectedTip = 0;
 
 
+let selectedTip = '';
 
-function countBill() {
+btnsTipNumber.forEach((btnTipNumber) =>
+  btnTipNumber.addEventListener('click', (e) => {
+    e.preventDefault();
+    selectedTip = Number.parseInt(e.target.dataset.tip);
+    // console.log(e.target.dataset.tip + " event target");
+    // console.log(selectedTip + " selected TIP");
+    console.log(selectedTip)
+    return selectedTip
+  })
+)
+
+const countTipCustom = function () {
+  // if (tipNumberCustom.value > '100') {
+
+  // }
+  selectedTip = tipNumberCustom.value;
+  console.log(selectedTip)
+  return selectedTip
+}
+
+
+const countBill = function () {
   if (!integer.test(billNumber.value) && billNumber.value !== '0') {
     console.log('hi')
   } else if (billNumber.value === '0') {
     console.log('not 0')
   }
-  return (billNumber)
+  console.log(billNumber.value)
+  return (billNumber.value)
 }
 
 /**
  * Check whether people's number is legible
  */
-function countPeople() {
+const countPeople = function () {
   if (peopleNumber.value === '0') {
     warningMessage.classList.remove('alert__info--hidden')
     document.querySelector('.input--hidden').style.border = '3px solid #dc8f78'
   } else {
     warningMessage.classList.add('alert__info--hidden')
     document.querySelector('.input--hidden').style.border = '3px solid #a0e7df'
+    console.log(peopleNumber.value)
+    return (peopleNumber.value)
   }
 }
 
-function countTipCustom() {
-  if (tipNumberCustom.value > '100') {
 
-  }
-  tip = tipNumberCustom.value
-}
 
 function calculation() {
-  return ((billNumber.value * (tip / 100)) / peopleNumber.value).toFixed(2);
+  if (billNumber.value !== '' && selectedTip !== '' && peopleNumber.value === '') {
+    console.log('no people')
+    warningMessage.classList.remove('alert__info--hidden')
+    document.querySelector('.input--hidden').style.border = '3px solid #dc8f78'
+  } else if (billNumber.value !== '' && selectedTip !== '' && peopleNumber.value !== '') {
+    warningMessage.classList.add('alert__info--hidden')
+    document.querySelector('.input--hidden').style.border = '3px solid #a0e7df'
+    // result1 = ((billNumber.value * (tip / 100)) / peopleNumber.value).toFixed(2);
+    console.log(billNumber.value)
+    console.log(result1)
+    // personAmount.innerHTML = `${result1}`
+  }
+  console.log('no people')
 }
+
+
+// function
 
 function resetAll() {
   billNumber.value = '';
